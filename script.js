8c5364a75dd2a58c28cfaafba23ed1eb2e1398ea
GrandPredator <atharvskotwal@gmail.com>
2024-10-06 00:30:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,10 @@ backToTopButton.onclick = function() {
         behavior: 'smooth'
     });
 };
+
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active'); // Toggle the active class
+};
+
+
